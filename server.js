f0da79f21535d184ccc87750904af0c12047a415
Andres Antillon <aantillon.leiva@gmail.com>
2020-07-07 14:51:25 -0600

    update server.js

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.get("/", (request, response) => {
 });
 
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
